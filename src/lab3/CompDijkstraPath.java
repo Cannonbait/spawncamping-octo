b7a3a8e90c5d9be31d6294e45fce26ce77ad4da1
Cannonbait <ivar_busig@hotmail.com>
2015-03-03 22:01:25 +0100

    Added internal class comparator

--- src/lab3/CompDijkstraPath.java
+++ src/lab3/CompDijkstraPath.java
@@ -1,15 +1,22 @@
 
 package lab3;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 
 public class CompDijkstraPath<E extends Edge> {
-    
+    private final int size;
+    private final PriorityQueue<E> queue;
     
     public CompDijkstraPath(List<E> edges){
-        //Create Dijkstra tree thing
+        size = edges.size();
+        queue = new PriorityQueue(size, new EdgeComparator());
+        for(E e:edges){
+            queue.add(e);
+        }
     }
     
     public Iterator<E> getShortest(int from, int to){
@@ -17,4 +24,22 @@ public class CompDijkstraPath<E extends Edge> {
     }
     
     
+    
+    
+}
+
+class EdgeComparator<E extends Edge> implements Comparator{
+    @Override
+    public int compare(Object o1, Object o2) {
+        final double first, second;
+        first = ((E)o1).getWeight();
+        second = ((E)o2).getWeight();
+        if (first < second){
+            return -1;
+        } else if (first == second){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }

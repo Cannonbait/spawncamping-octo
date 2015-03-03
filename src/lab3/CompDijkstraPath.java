fd6c1214336c7441f56a5058fc0965cbbc024f33
Cannonbait <ivar_busig@hotmail.com>
2015-03-03 16:46:09 +0100

    Added Graph classes

--- src/lab3/CompDijkstraPath.java
+++ src/lab3/CompDijkstraPath.java
@@ -1,7 +1,20 @@
 
 package lab3;
 
+import java.util.Iterator;
+import java.util.List;
 
-public class CompDijkstraPath {
 
+public class CompDijkstraPath<E extends Edge> {
+    
+    
+    public CompDijkstraPath(List<E> edges){
+        //Create Dijkstra tree thing
+    }
+    
+    public Iterator<E> getShortest(int from, int to){
+        return null;
+    }
+    
+    
 }
--- src/lab3/CompKruskalEdge.java
+++ src/lab3/CompKruskalEdge.java
@@ -1,7 +1,18 @@
 
 package lab3;
 
+import java.util.Iterator;
+import java.util.List;
 
-public class CompKruskalEdge {
+
+public class CompKruskalEdge<E extends Edge> {
+    
+    public CompKruskalEdge(List<E> edges){
+        
+    }
+    
+    public Iterator<E> getMinimumSpanningTree(){
+        return null;
+    }
 
 }

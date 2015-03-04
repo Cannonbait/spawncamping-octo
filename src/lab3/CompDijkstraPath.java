3bbb71c6d628dbafae34af556821559a3b587d36
Cannonbait <ivar_busig@hotmail.com>
2015-03-04 14:16:56 +0100

    Finished work on Dijkstra

--- src/lab3/CompDijkstraPath.java
+++ src/lab3/CompDijkstraPath.java
@@ -1,7 +1,7 @@
 
 package lab3;
 
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -9,37 +9,90 @@ import java.util.PriorityQueue;
 
 public class CompDijkstraPath<E extends Edge> {
     private final int size;
-    private final PriorityQueue<E> queue;
+    private final PriorityQueue<QueueElement> queue;
+    private final List<E> edges;
+    private final List<Integer> visited;
     
     public CompDijkstraPath(List<E> edges){
         size = edges.size();
-        queue = new PriorityQueue(size, new EdgeComparator());
-        for(E e:edges){
-            queue.add(e);
-        }
+        queue = new PriorityQueue(size-1);
+        this.edges = edges;
+        visited = new ArrayList<>();
+        
     }
     
     public Iterator<E> getShortest(int from, int to){
+        for (E edge:edges){
+            if (edge.getSource() == from){
+                List<E> path = new ArrayList<>();
+                path.add(edge);
+                queue.add(new QueueElement(edge, path));
+            }
+        }
+        visited.add(from);
+        
+        while (!queue.isEmpty()){
+            QueueElement next = queue.poll();
+            final int dest = next.getDest();
+            if (!visited.contains(dest)){
+                if (next.getDest() == to){
+                    return next.getPath().iterator();
+                } else {
+                    visited.add(dest);
+                    for (E edge:edges){
+                        if (!visited.contains(edge.getDest())){
+                            List<E> oldPath = (List<E>)next.getPath();
+                            oldPath.add(edge);
+                            queue.add(new QueueElement(edge, oldPath));
+                        }
+                    }
+                }   
+            }   
+        }
         return null;
     }
-    
-    
-    
-    
 }
 
-class EdgeComparator<E extends Edge> implements Comparator{
+class QueueElement<E extends Edge> implements Comparable<QueueElement>{
+    private final E edge;
+    private final List<E> path;
+    
+    public QueueElement(E e, List<E> path){
+        this.edge = e;
+        this.path = new ArrayList<>(path);
+    }
+
     @Override
-    public int compare(Object o1, Object o2) {
-        final double first, second;
-        first = ((E)o1).getWeight();
-        second = ((E)o2).getWeight();
-        if (first < second){
+    public int compareTo(QueueElement toCompare) {
+        double compareWeight = toCompare.getWeight();
+        if (edge.getWeight() < compareWeight){
             return -1;
-        } else if (first == second){
+        } else if (edge.getWeight() == compareWeight){
             return 0;
         } else {
             return 1;
         }
     }
+    
+    public double getWeight(){
+        return edge.getWeight();
+    }
+    
+    public int getSource(){
+        return edge.getSource();
+    }
+
+    public int getDest(){
+        return edge.getDest();
+    }
+    
+    public List<E> getPath(){
+        return new ArrayList<E>(path);
+    }
+    
+    public E getEdge(){
+        return edge;
+    }
 }
+
+
